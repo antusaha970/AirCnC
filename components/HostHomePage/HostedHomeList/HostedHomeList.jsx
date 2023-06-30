714dcfc81fdecc0588aaadd4fd919903835a2389
Antu Saha <antusaha970@gmail.com>
2023-06-30 13:32:45 +0600

    added fetch data

--- components/HostHomePage/HostedHomeList/HostedHomeList.jsx
+++ components/HostHomePage/HostedHomeList/HostedHomeList.jsx
@@ -2,11 +2,26 @@
 
 import { useUser } from "@clerk/nextjs";
 import { Box, Typography } from "@mui/material";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+export const fetchCache = "force-no-store";
 const HostedHomeList = () => {
   const {
     user: { id },
   } = useUser();
+  const [homes, setHomes] = useState([]);
+  useEffect(() => {
+    fetchData();
+    async function fetchData() {
+      try {
+        const { data } = await axios.get(`/api/order/get-hosted-places/${id}`);
+        setHomes(data.hostedPlaces);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, [id]);
+  console.log(homes);
   return (
     <Box
       sx={{
