e0d74d1a5ecf0dc37e69cfea498a7ffb01ae79d0
Antu Saha <antusaha970@gmail.com>
2023-07-13 00:37:38 +0600

    added cursodel

--- components/PlaceDetailsPage/PlaceLanding/PlaceLanding.jsx
+++ components/PlaceDetailsPage/PlaceLanding/PlaceLanding.jsx
@@ -1,9 +1,57 @@
 "use client";
 
 import { Box } from "@mui/material";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Image from "next/image";
+import { settings } from "@utils/helperFunctions";
 
 const PlaceLanding = ({ place }) => {
-  return <Box component="section"></Box>;
+  const {
+    result: {
+      placeDetails: { images },
+    },
+  } = place;
+  console.log(place);
+
+  return (
+    <Box component="section">
+      <Box
+        sx={{
+          width: "100%",
+          minHeight: "60vh",
+          overflow: "hidden",
+          mt: 1,
+        }}
+      >
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <div key={image}>
+              <Box
+                sx={{
+                  position: "relative",
+                  width: { sm: "100%", md: "600px" },
+                  height: "50vh",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  margin: "auto",
+                }}
+              >
+                <Image
+                  src={images[index]}
+                  alt="corusel image"
+                  fill
+                  sizes="(min-width: 768px) 100%, (min-width: 1200px) 100%, 100%"
+                />
+              </Box>
+            </div>
+          ))}
+        </Slider>
+      </Box>
+    </Box>
+  );
 };
 
 export default PlaceLanding;
--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -40,4 +40,14 @@ const formatSearchOptions = (
   return searchOptionsObject;
 };
 
-export { formatSearchOptions };
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+};
+
+export { formatSearchOptions, settings };
